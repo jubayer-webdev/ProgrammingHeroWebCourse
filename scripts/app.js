2a5729eabaa7d1586a9c5709c47451d5da898c06
Jame___boy <jjame0030@gmail.com>
2024-03-04 22:26:25 +0600

    mobile responsive is done in letdiscuss seciton

--- scripts/app.js
+++ scripts/app.js
@@ -24,37 +24,41 @@ const displayCards = (searchKey) => {
                 // console.log('searchkey = ', searchKey);
                 const newCard = document.createElement('div');
                 newCard.className = 'bg-[#7B7DFC1A]';
-                newCard.innerHTML = `
-                <div class="w-20 h-20">
-                        <img src=${posts[i].image} alt="" />
-                    </div>
-                    <div class="space-y-1">
-                        <p class="mr-4"># ${posts[i].category}</p>
-                        <p>Author : ${posts[i].author.name}</p>
-                        <h4 class="text-xl text-[#12132D] font-bold">${posts[i].title}</h4>
-                        <p class="text-base text-[#12132D99] font-bold">It’s one thing to subject yourself to ha Halloween costume mishap because, hey that’s your prerogative</p>
-                        <p class="bg-[#12132D40] border-b-2 border-dashed"></p>
+                //!  https://daisyui.com/components/indicator/
+                newCard.innerHTML =
+                    `
+                <div class="indicator">
+                    <span class="indicator-item badge ${posts[i].isActive ? 'badge-accent' : 'badge-secondary'} badge-xs"></span>
+                    <div class="grid w-20 h-20 bg-base-300 place-items-center rounded-2xl"><img src=${posts[i].image} alt="" /></div>
+                </div>
 
-                        <div class="flex justify-between">
-                            <div class="dbg-blue flex w-full space-x-8">
-                                <div class="flex space-x-3">
-                                    <div><img src="./images/message-2.svg" alt="" /></div>
-                                    <p>${posts[i].comment_count}</p>
-                                </div>
-                                <div class="flex space-x-3">
-                                    <div><img src="./images/eye.svg" alt="" /></div>
-                                    <p>${posts[i].view_count}</p>
-                                </div>
-                                <div class="flex space-x-3">
-                                    <div><img src="./images/clock-hour-9.svg" alt="" /></div>
-                                    <p>${posts[i].posted_time} min</p>
-                                </div>
+                <div class="space-y-1">
+                    <p class="mr-4"># ${posts[i].category}</p>
+                    <p>Author : ${posts[i].author.name}</p>
+                    <h4 class="text-xl text-[#12132D] font-bold">${posts[i].title}</h4>
+                    <p class="text-base text-[#12132D99] font-bold">It’s one thing to subject yourself to ha Halloween costume mishap because, hey that’s your prerogative</p>
+                    <p class="bg-[#12132D40] border-b-2 border-dashed"></p>
+
+                    <div class="flex justify-between">
+                        <div class="dbg-blue flex w-full space-x-8">
+                            <div class="flex space-x-3">
+                                <div><img src="./images/message-2.svg" alt="" /></div>
+                                <p>${posts[i].comment_count}</p>
                             </div>
-                            <div id="read-btn" class="dbg-tomato">
-                                <button onclick="titleAdd(${i})"><img src="./images/email.svg" alt="" /></button>
+                            <div class="flex space-x-3">
+                                <div><img src="./images/eye.svg" alt="" /></div>
+                                <p>${posts[i].view_count}</p>
+                            </div>
+                            <div class="flex space-x-3">
+                                <div><img src="./images/clock-hour-9.svg" alt="" /></div>
+                                <p>${posts[i].posted_time} min</p>
                             </div>
                         </div>
+                        <div id="read-btn" class="dbg-tomato">
+                            <button onclick="titleAdd(${i})"><img src="./images/email.svg" alt="" /></button>
+                        </div>
                     </div>
+                </div>
                 `;
                 cardContainer.appendChild(newCard);
             }
