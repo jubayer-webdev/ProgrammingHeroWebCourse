49665d1571a905fbb70c17d782635faca4a2aab8
Jame___boy <jjame0030@gmail.com>
2024-03-06 14:08:45 +0600

    36_5-3 A Simple Shopping Cart To Add Product And Quantity done

---
+++ js/cart.js
@@ -0,0 +1,19 @@
+const addProduct = () => {
+    const productField = document.getElementById('product-name');
+    const quantityField = document.getElementById('product-quantity');
+    const product = productField.value;
+    const quantity = quantityField.value;
+    productField.value = '';
+    quantityField.value = '';
+    console.log(product, quantity);
+    //!calling the displayProduct
+    displayProduct(product, quantity); 
+}
+
+//!to show the product, quantity in web page
+const displayProduct = (product, quantity) => {
+    const ul = document.getElementById('product-container');
+    const li = document.createElement('li');
+    li.innerText = `${product}: ${quantity}`;
+    ul.appendChild(li);
+}
