6088cd217e9763f8e743932034e0fb72532a5daf
Jame___boy <jjame0030@gmail.com>
2024-03-06 14:54:30 +0600

    36_5-4 (Advanced) Local Storage Interactions For A Shopping Cart is done

--- js/cart.js
+++ js/cart.js
@@ -7,13 +7,51 @@ const addProduct = () => {
     quantityField.value = '';
     console.log(product, quantity);
     //!calling the displayProduct
-    displayProduct(product, quantity); 
+    displayProduct(product, quantity);
+    //!to save product,quantity on localStorage
+    saveProductToLocalStorage(product, quantity);
 }
 
-//!to show the product, quantity in web page
+//!to show the product, quantity in web page dynamically
 const displayProduct = (product, quantity) => {
     const ul = document.getElementById('product-container');
     const li = document.createElement('li');
     li.innerText = `${product}: ${quantity}`;
     ul.appendChild(li);
 }
+
+//!for getting the previous cart data
+const getStoredShoppingCart = () => {
+    const storedCart = localStorage.getItem('cart');
+    let cart = {};
+    if (storedCart) {
+        //!convert JSON to object
+        cart = JSON.parse(storedCart);
+    }
+    return cart;
+}
+
+//!to set product,quantity on localStorage
+const saveProductToLocalStorage = (product, quantity) => {
+    const cart = getStoredShoppingCart();
+    cart[product] = quantity;
+    const cartStringified = JSON.stringify(cart);
+    console.log(`JSON.stringify(cart) = ${cartStringified}`);
+
+    //!set on localStorage
+    localStorage.setItem('cart', cartStringified);
+}
+
+//!to show localStorage data on reload, or another tab in same browser
+const displayProductsFromLocalStorage = () => {
+    const savedCart = getStoredShoppingCart();
+    console.log(savedCart);
+    for (const product in savedCart) {
+        const quantity = savedCart[product];
+        console.log(product, quantity);
+        //!show on web page when reload or another tab
+        displayProduct(product, quantity);
+    }
+}
+//!calling the function on reload, or another tab in same browser
+displayProductsFromLocalStorage();
