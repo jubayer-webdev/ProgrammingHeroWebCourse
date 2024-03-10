c2f926b2e5f25b8b0428492b362dac42985ad5ac
Jame___boy <jjame0030@gmail.com>
2024-03-10 23:47:36 +0600

    40-2 Display Countries In A Simple Way, Folder Structure is DONE

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,16 +1,22 @@
 import { useState } from "react";
 import { useEffect } from "react";
+import Country from "../Country/Country";
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
+    //! 40-1 video
     useEffect(() => {
-        fetch('https://restcountries.com/v3.1/all')
-        .then(res => res.json())
-        .then(data => console.log(data))
+        fetch("https://restcountries.com/v3.1/all")
+            .then((res) => res.json())
+            .then((data) => setCountries(data));
     }, []);
     return (
         <div>
-            <h3>Countries</h3>
+            <h3>Countries {countries.length}</h3>
+            {/* //! 40-2 video */}
+            {countries.map((country) => (
+                <Country key={country.cca3} country={country}></Country>
+            ))}
         </div>
     );
 };
