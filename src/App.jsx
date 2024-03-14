632695a9e71993a15d912e9fd8ac810f36bbc872
Jame___boy <jjame0030@gmail.com>
2024-03-14 08:29:53 +0600

    42-5 Add Bookmark Button And Lift Up The State && React icon from https://react-icons.github.io/react-icons/search/#q= link

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 import Blogs from "./components/Blogs/Blogs";
 import Bookmarks from "./components/Bookmarks/Bookmarks";
 import Header from "./components/Header/Header";
 
 function App() {
+    const [bookmarks, setBookmarks] = useState([]);
+
+    const handleAddToBookMark = (blog) => {
+        console.log("bookmark adding soon...");
+    };
+
     return (
         <>
             <Header></Header>
-            <div className="md:flex">
-                <Blogs></Blogs>
+            <div className="md:flex max-w-7xl mx-auto">
+                <Blogs handleAddToBookMark={handleAddToBookMark}></Blogs>
                 <Bookmarks></Bookmarks>
             </div>
         </>
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import Blog from "../Blog/Blog";
 
-const Blogs = () => {
+const Blogs = ({ handleAddToBookMark }) => {
     const [blogs, setBlogs] = useState([]);
 
     useEffect(() => {
@@ -14,7 +14,7 @@ const Blogs = () => {
         <div className="md:w-2/3">
             <h1 className="text-4xl">Blogs: {blogs.length}</h1>
             {blogs.map((blog) => (
-                <Blog key={blog.id} blog={blog}></Blog>
+                <Blog key={blog.id} blog={blog} handleAddToBookMark={handleAddToBookMark}></Blog>
             ))}
         </div>
     );
