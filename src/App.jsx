ce8e6bd8705c478fa516b85ee7429b737ca0b468
Jame___boy <jjame0030@gmail.com>
2024-03-19 10:23:26 +0600

    banner added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Banner from "./components/Banner/Banner"
 
 function App() {
     return (
         <>
             <Header></Header>
-            {/* <Banner></Banner> */}
+            <Banner></Banner>
         </>
     );
 }
