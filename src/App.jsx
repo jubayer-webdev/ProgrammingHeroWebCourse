301c953b668b1422c72e89a16cfab791886173dc
Jame___boy <jjame0030@gmail.com>
2024-03-19 10:34:06 +0600

    sectionTitle is done with responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Banner from "./components/Banner/Banner"
+import SectionTitle from "./components/SectionTitle/SectionTitle";
 
 function App() {
     return (
         <>
             <Header></Header>
             <Banner></Banner>
+            <SectionTitle title="Our Recipes" subtitle="Unlock Flavorful Delights: Dive into Our Exquisite Recipe Collection!"></SectionTitle>
         </>
     );
 }
