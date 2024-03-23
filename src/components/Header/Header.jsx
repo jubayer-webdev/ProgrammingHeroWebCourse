17f1df737d46259b6ac5ebee0c2da9c580a63754
Jame___boy <jjame0030@gmail.com>
2024-03-24 00:57:28 +0600

    45-8 Active Route (<NavLink></NavLink>), Loading Spinner (useNavigation), useLocation()

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./Header.css";
 const Header = () => {
     return (
         <div>
             <nav>
                 <span>My Website</span>
+                {/* <a href="/">Home</a> */}
                 {/* //!Using <Link> for avoiding reload page */}
                 <Link to="/">Home</Link>
-                <Link to="/users">Users</Link>
-                <Link to="/posts">Posts</Link>
-                {/* <a href="/">Home</a> */}
-                <Link to="/about">About</Link>
+                {/* <Link to="/users">Users</Link> */}
+                {/* //!https://reactrouter.com/en/main/start/tutorial#active-link-styling */}
+                <NavLink to="/users">Users</NavLink>
+                {/* <Link to="/posts">Posts</Link> */}
+                {/* //!When use navlink, class='active' will automatically add */}
+                <NavLink to="/posts">Posts</NavLink>
                 {/* <a href="/about">About</a> */}
-                <Link to="/contact">Contact</Link>
+                <Link to="/about">About</Link>
                 {/* <a href="/contact">Contact Us</a> */}
+                <Link to="/contact">Contact</Link>
             </nav>
         </div>
     );
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
 const Home = () => {
+    const navigation = useNavigation();
+    const location = useLocation();
+    console.log("location", location);
+
     return (
         <div>
             <Header></Header>
             {/* //!Where to show children:[], কোন রাউটারে গেলে কি দেখাবো সেটার জন্য <Outlet/> */}
-            <Outlet></Outlet>
+            {navigation.state === "loading" ? <p>Loading... or add spinner </p> : <Outlet></Outlet>}
             <Footer></Footer>
         </div>
     );
