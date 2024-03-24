057bc41ec4d220edd937374076e0a2eea61bbc37
Jame___boy <jjame0030@gmail.com>
2024-03-24 17:31:49 +0600

    46-4 Custom Hook And Create Your First Custom Hook, return an object{} & destructuring

--- src/components/CustomHookForm/CustomHookForm.jsx
+++ src/components/CustomHookForm/CustomHookForm.jsx
@@ -1,21 +1,29 @@
 import useInputState from "../hooks/useInputState";
 
 const CustomHookForm = () => {
-    const [name, handleNameChange] = useInputState("defaultName");
-    console.log(useInputState('jame'));
+    // const [name, handleNameChange] = useInputState("defaultName");
+    // console.log(useInputState("jame"));
+
+    const emailState = useInputState("default@email.go");
+    console.log("emailState = ", emailState);
+
+    // const {value, onChange} = {...emailState};
+    // console.log(value);
+    // console.log(onChange
 
     const handleSubmit = (e) => {
-        console.log('CustomHookForm name = ', name);
-        console.log('handleNameChange = ', handleNameChange);
+        // console.log("CustomHookForm name = ", name);
+        // console.log("handleNameChange = ", handleNameChange);
+        console.log("emailState.value = ", emailState.value);
         e.preventDefault();
     };
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input value={name} onChange={handleNameChange} type="text" name="firstInput" />
+                {/* <input value={name} onChange={handleNameChange} type="text" name="firstInput" /> */}
                 <br />
-                <input type="email" name="secondInput" />
+                <input {...emailState} type="email" name="secondInput" />
                 <br />
                 <input type="password" name="password" />
                 <br />
--- src/components/hooks/useInputState.js
+++ src/components/hooks/useInputState.js
@@ -3,11 +3,19 @@ import { useState } from "react"
 const useInputState = (defaultValue = null) => {
     const [value, setValue] = useState(defaultValue);
 
-    const handleChange = (evnt) => {
-        setValue(evnt.target.value);
-        console.log('In useInputState value = ', value);
+    // const handleChange = (evnt) => {
+    //     setValue(evnt.target.value);
+    //     console.log('In useInputState value = ', value);
+    // }
+    // return [value, handleChange];
+
+    //! return by object
+    const onChange = e => {
+        setValue(e.target.value);
+        console.log('onChange value = ', value);
     }
-    return [value, handleChange];
+    //! {value: "default@email.go", onChange:f}
+    return { value, onChange };
 }
 
 export default useInputState;
