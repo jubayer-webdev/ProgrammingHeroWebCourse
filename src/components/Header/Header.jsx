bac1f2b4d9508c4cfb17d367dd24b54482b11126
Jame___boy <jjame0030@gmail.com>
2024-03-28 17:36:16 +0600

    fixed Book Vibe issue on responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -26,26 +26,35 @@ const Header = () => {
 
     return (
         //! https://daisyui.com/components/navbar/
-        <div className="navbar bg-base-100 mb-24">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
-                        </svg>
+        <div className="mb-24">
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+                            </svg>
+                        </div>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            {/* //!Here the menu */}
+                            {links}
+                        </ul>
                     </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <a className="hidden lg:block text-green-500 font-extrabold text-xl">Book Vibe</a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {/* //!Here the menu */}
                         {links}
                     </ul>
                 </div>
-                <a className="text-green-500 font-extrabold text-xl">Book Vibe</a>
+                <div className="navbar-end">
+                    <a className="btn btn-accent mr-4 px-7 py-4">Sign In</a>
+                    <a className="btn bg-[#59C6D2] px-7 py-4">Sign Up</a>
+                </div>
             </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">{links}</ul>
-            </div>
-            <div className="navbar-end">
-                <a className="btn btn-accent mr-4 px-7 py-4">Sign In</a>
-                <a className="btn bg-[#59C6D2] px-7 py-4">Sign Up</a>
+            <div className="block lg:hidden text-center">
+                <a className="text-green-500 font-extrabold text-4xl">Book Vibe</a>
             </div>
         </div>
     );
