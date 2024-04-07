9a2868a644565a41a25503a179d99c9545517128
Jame___boy <jjame0030@gmail.com>
2024-04-07 12:35:21 +0600

    set up auth in Login components

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,9 @@
+import { getAuth } from "firebase/auth";
+import app from "../../firebase/firebase.init";
+
 const Login = () => {
+    // Initialize Firebase Authentication and get a reference to the service
+    const auth = getAuth(app);
     return (
         <div>
             <button>Google Login</button>
