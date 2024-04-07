541aa02a4c05c05e72f80e98f76f8047cab0ac5f
Jame___boy <jjame0030@gmail.com>
2024-04-07 15:22:21 +0600

    added sign out button and functionality

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 import app from "../../firebase/firebase.init";
 import { useState } from "react";
 
@@ -10,6 +10,7 @@ const Login = () => {
 
     //!Should set up sign-in-method google in firebase to avoid Firebase:Error(auth/configuration-not-found
     const handleGoogleSignIn = () => {
+        //!https://firebase.google.com/docs/auth/web/google-signin?hl=en&authuser=0
         signInWithPopup(auth, provider)
             .then((result) => {
                 const loggedInUser = result.user;
@@ -21,9 +22,22 @@ const Login = () => {
             });
     };
 
+    const handleSignOut = () =>{
+        signOut(auth)
+            .then(() => {
+                // Sign-out successful.
+                setUser(null);
+            })
+            .catch((error) => {
+                // An error happened.
+                console.log('error on Sign Out...',error);
+            });
+    }
+
     return (
         <div>
             <button onClick={handleGoogleSignIn}>Google Login</button>
+            <button onClick={handleSignOut}>Sign Out</button>
             {/* //! Conditional rendering */}
             {user && (
                 <div>
