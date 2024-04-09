ba4008789a6a2a2e09998b4513ba3cbc9e4044bb
Jame___boy <jjame0030@gmail.com>
2024-04-10 01:15:33 +0600

    added register form

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,7 +1,16 @@
 const Register = () => {
     return (
-        <div>
-            <h2 className="text-3xl">Please Register</h2>
+        <div className="border">
+            <div className="mx-auto md:w-1/2 border">
+                <h2 className="text-3xl mb-8">Please Register</h2>
+                <form>
+                    <input className="mb-4 w-3/4 py-2 px-4" type="email" placeholder="Email Address" name="emaiL" id="" />
+                    <br />
+                    <input className="mb-4 w-3/4 py-2 px-4" type="password" placeholder="Password" name="passworD" id="" />
+                    <br />
+                    <input className="btn btn-secondary mb-4 w-3/4" type="submit" value="Register" />
+                </form>
+            </div>
         </div>
     );
 };
