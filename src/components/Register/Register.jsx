9b6789639d35dbd5bf54c867dba67c47995e5bcf
Jame___boy <jjame0030@gmail.com>
2024-04-10 01:25:25 +0600

    added handleRegister

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,17 @@
 const Register = () => {
+    const handleRegister = (e) => {
+        //! prevent reload the page when click on submit
+        e.preventDefault();
+        const email = e.target.emaiL.value;
+        const password = e.target.passworD.value;
+        console.log(email, password);
+    };
+
     return (
         <div className="border">
             <div className="mx-auto md:w-1/2 border">
                 <h2 className="text-3xl mb-8">Please Register</h2>
-                <form>
+                <form onSubmit={handleRegister}>
                     <input className="mb-4 w-3/4 py-2 px-4" type="email" placeholder="Email Address" name="emaiL" id="" />
                     <br />
                     <input className="mb-4 w-3/4 py-2 px-4" type="password" placeholder="Password" name="passworD" id="" />
