40933809748b45fc0f8c14e31b7adec9e3fc6025
Jame___boy <jjame0030@gmail.com>
2024-04-10 15:33:01 +0600

    50-6 Login User And Accept Terms And Conditions

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,65 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import auth from "../../firebase/firebase.config";
+import { useState } from "react";
+
 const Login = () => {
+    const [registerError, setRegisterError] = useState("");
+
+    const handleLogin = (e) => {
+        //! prevent reload the page when click on submit
+        e.preventDefault();
+        //! Reset Error
+        setRegisterError("");
+
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log("from Login...", email, password);
+
+        signInWithEmailAndPassword(auth, email, password)
+            .then((result) => console.log("From Login user...", result.user))
+            .catch((error) => {
+                console.error("From Login...", error);
+
+                //! set error
+                setRegisterError(error.message);
+            });
+    };
+
     return (
-        <div>
-            <h2 className="text-3xl">Please Login</h2>
+        //! https://daisyui.com/components/hero/
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                </div>
+                <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handleLogin} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">
+                                    Forgot password?
+                                </a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                    </form>
+                    {/* //!Showing The Error Message */}
+                    {registerError && <p className="text-red-700">{registerError}</p>}
+                </div>
+            </div>
         </div>
     );
 };
