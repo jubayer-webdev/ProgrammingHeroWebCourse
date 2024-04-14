e28f32fdbb1c1966914d1e1fd16aa1fecf589a01
Jame___boy <jjame0030@gmail.com>
2024-04-14 23:23:49 +0600

    added github link of ph on README.md file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
                         {navLinks}
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="btn btn-ghost text-xl">Auth M9Module51</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
