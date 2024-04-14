b23c97dfb027554be3516b46b92d5d96bddff2bf
Jame___boy <jjame0030@gmail.com>
2024-04-14 17:57:08 +0600

    create Navbar and set it to Root.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,28 @@ import "./index.css";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./components/Root";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Root></Root>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>,
+            },
+            {
+                path: "/login",
+                element: <Login></Login>,
+            },
+            {
+                path: "/register",
+                element: <Register></Register>,
+            },
+        ],
     },
 ]);
 
