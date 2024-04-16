e1ea0318bf047c672315dee73603479ab8ae04ea
Jame___boy <jjame0030@gmail.com>
2024-04-16 15:20:25 +0600

    remove field data from login and register from after login or regitster

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -19,6 +19,7 @@ const Register = () => {
         createUser(email, password)
             .then((result) => {
                 console.log(result.user);
+                e.target.reset();
             })
             .catch((error) => {
                 console.error(error);
