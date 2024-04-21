964a4723758c2cfdbd2ef5f32c0183ead3267b9b
Jame___boy <jjame0030@gmail.com>
2024-04-21 22:39:42 +0600

    55-5 Delete A User From The Database Using UI Click Button

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,7 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://jjame0030:zGS6q2rphN46W7C7@cluster0.2ncikoy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -37,11 +37,22 @@ async function run() {
         })
 
         //! create a route for post
+        //! https://www.mongodb.com/docs/drivers/node/current/usage-examples/insertOne/
         app.post('/users', async (req, res) => {
             const user = req.body;
             console.log('new user backend =', user);
             const result = await userCollection.insertOne(user);
 
+            res.send(result);
+        })
+
+        //! https://www.mongodb.com/docs/drivers/node/current/usage-examples/deleteOne/
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('please delete from database', id);
+
+            const query = { _id: new ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
             res.send(result);
         })
 
