f3a3bcdf468a4596bb71c55d93423f2c270e8744
Jame___boy <jjame0030@gmail.com>
2024-04-21 23:20:32 +0600

    set nested route, created component, install tailwind (by me)

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,29 @@ import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./layouts/RootLayout";
 import Users from "./pages/Users";
+import CreateAndDeleteUser from "./pages/CreateAndDeleteUser";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <RootLayout></RootLayout>,
+        children: [
+            {
+                path: "/",
+                element: <CreateAndDeleteUser></CreateAndDeleteUser>,
+            },
+            {
+                path: "/users",
+                element: <Users></Users>,
+                loader: () => fetch("http://localhost:5000/users"),
+            },
+        ],
     },
-    {
-        path: "/users",
-        element: <Users></Users>,
-        loader: () => fetch("http://localhost:5000/users"),
-    },
+    // {
+    //     path: "/users",
+    //     element: <Users></Users>,
+    //     loader: () => fetch("http://localhost:5000/users"),
+    // },
 ]);
 
 function App() {
---
+++ src/pages/CreateAndDeleteUser.jsx
@@ -0,0 +1,46 @@
+const CreateAndDeleteUser = () => {
+    const handleAddUser = (event) => {
+        event.preventDefault();
+
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const user = { name, email };
+        console.log(user);
+
+        //! send new data to server
+        //! https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#uploading_json_data
+        fetch("http://localhost:5000/users", {
+            method: "POST", //(case insensitive) or 'PUT'
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+
+                if (data.insertedId) {
+                    alert("Users added successfully");
+                    form.reset();
+                }
+            });
+    };
+    return (
+        <div>
+            <h1>Simple CRUD</h1>
+
+            {/* //!create form */}
+            <form onSubmit={handleAddUser}>
+                <input type="text" name="name" id="" />
+                <br />
+                <input type="email" name="email" id="" />
+                <br />
+                <input type="submit" value="Add User" />
+            </form>
+        </div>
+    );
+};
+
+export default CreateAndDeleteUser;
