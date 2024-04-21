1f17811e5b36ba771f11237abdd8159505cee560
Jame___boy <jjame0030@gmail.com>
2024-04-21 19:50:38 +0600

    55-4 Read Data, Find Multiple User Data And Display On UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import "./App.css";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./layouts/RootLayout";
+import Users from "./pages/Users";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <RootLayout></RootLayout>,
+    },
+    {
+        path: "/users",
+        element: <Users></Users>,
+        loader: () => fetch("http://localhost:5000/users"),
     },
 ]);
 
