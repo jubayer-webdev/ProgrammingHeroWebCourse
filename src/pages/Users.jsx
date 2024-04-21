8ead52ba50d81acf5ac94805cb7a7851217a0585
Jame___boy <jjame0030@gmail.com>
2024-04-21 22:47:41 +0600

    55-5 Delete A User From The Database Using UI Click Button

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -3,13 +3,30 @@ import { useLoaderData } from "react-router-dom";
 const Users = () => {
     const users = useLoaderData();
 
+    const handleDeleteUser = (id) => {
+        console.log("delete", id);
+
+        fetch(`http://localhost:5000/users/${id}`, {
+            method: "DELETE",
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                if(data.deletedCount > 0){
+                    alert("Successfully deleted one document.");
+                }
+            });
+    };
+
     return (
         <div>
             <h2>total users: {users.length}</h2>
             <div>
-                {
-                    users.map(user => <p key={user._id}>{user.name} : {user.email}</p>)
-                }
+                {users.map((user) => (
+                    <p key={user._id}>
+                        {user.name} : {user.email} ---- {user._id} <button onClick={() => handleDeleteUser(user._id)}>X</button>
+                    </p>
+                ))}
             </div>
         </div>
     );
