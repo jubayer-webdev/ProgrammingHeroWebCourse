7bf19d39e568b4a76704621f192897db2aa3c172
Jame___boy <jjame0030@gmail.com>
2024-04-22 06:22:47 +0600

    55-7 Load Single Item By Id And Create Update Route, create form

--- src/pages/Update.jsx
+++ src/pages/Update.jsx
@@ -1,7 +1,28 @@
+import { useLoaderData } from "react-router-dom";
+
 const Update = () => {
+    const loadedUser = useLoaderData();
+
+    const handleUpdate = (event) => {
+        event.preventDefault();
+
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        console.log(name, email);
+    };
+
     return (
         <div>
-            <h3>Update</h3>
+            <h3 className="text-6xl mb-7">Update information of {loadedUser.name}</h3>
+            {/* //! form */}
+            <form onSubmit={handleUpdate}>
+                <input className="mb-2 p-2" type="text" name="name" defaultValue={loadedUser?.name} id="" />
+                <br />
+                <input className="mb-2 p-2" type="email" name="email" defaultValue={loadedUser?.email} id="" />
+                <br />
+                <input className="btn btn-success btn-outline" type="submit" value="Update" />
+            </form>
         </div>
     );
 };
