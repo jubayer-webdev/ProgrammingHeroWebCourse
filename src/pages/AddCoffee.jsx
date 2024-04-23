f2a8c267725eeff486349ceda14856d3b22fa99a
Jame___boy <jjame0030@gmail.com>
2024-04-24 02:22:07 +0600

    56-3 starting make a form in AddCoffee.jsx file

--- src/pages/AddCoffee.jsx
+++ src/pages/AddCoffee.jsx
@@ -2,6 +2,27 @@ const AddCoffee = () => {
     return (
         <div>
             <h2>Add a Coffee</h2>
+
+            <form>
+                <div className="flex ">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Coffee Name</span>
+                        </label>
+                        <label>
+                            <input className="input input-bordered" type="text" placeholder="Coffee Name" />
+                        </label>
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Available Quantity</span>
+                        </label>
+                        <label>
+                            <input className="input input-bordered" type="text" placeholder="Available Quantity" />
+                        </label>
+                    </div>
+                </div>
+            </form>
         </div>
     );
 };
