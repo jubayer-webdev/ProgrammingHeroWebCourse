e113e8faaf1fbf45af63f5cc8c28c8ed3f4763f5
Jame___boy <jjame0030@gmail.com>
2024-04-24 14:55:09 +0600

    Delete A Coffee

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -60,6 +60,17 @@ async function run() {
             const result = await coffeeCollection.insertOne(newCoffee);
             res.send(result);
         })
+
+        //* Delete just one document
+        // https://expressjs.com/en/starter/basic-routing.html
+        app.delete('/coffees/:id', async (req, res) => {
+            const id = req.params.id;
+            // https://www.mongodb.com/docs/drivers/node/current/usage-examples/deleteOne/
+            const query = { _id: new ObjectId(id) };
+            const result = await coffeeCollection.deleteOne(query);
+            res.send(result);
+        })
+
         //!----------------CRUD  End  ------------
 
 
