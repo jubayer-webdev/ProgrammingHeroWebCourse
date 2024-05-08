ec6b0ce9f575a64dfb12f25338c843b36a4850a6
Jame___boy <jjame0030@gmail.com>
2024-05-09 03:15:33 +0600

    update Navbar and Main

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 //! It is our RootLayout
 const Main = () => {
@@ -9,9 +10,14 @@ const Main = () => {
             <Navbar />
 
             {/*//! Outlet */}
-            <Outlet />
+            {/* Nav(62 + 8 + 8) + Footer(173 + 32 + 32) = 305 */}
+            {/*//! 100vh - 305px --- space is not allowed!!!  */}
+            <div className="min-h-[calc(100vh-305px)]">
+                <Outlet />
+            </div>
 
             {/*//! Footer */}
+            <Footer />
         </div>
     );
 };
