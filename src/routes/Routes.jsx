349fe8b76278a2a495604d3519a672b0ec1f2a0c
Jame___boy <jjame0030@gmail.com>
2024-05-09 23:47:47 +0600

    set route for JobDetails

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -5,6 +5,7 @@ import Main from "../layouts/Main";
 import Home from "../pages/Home";
 import Login from "../pages/Authentication/Login";
 import Register from "../pages/Authentication/Register";
+import JobDetails from "../pages/JobDetails";
 
 const router = createBrowserRouter([
     {
@@ -22,6 +23,10 @@ const router = createBrowserRouter([
             {
                 path: "/register",
                 element: <Register />,
+            },
+            {
+                path: "/job/:id",
+                element: <JobDetails />,
             },
         ],
     },
