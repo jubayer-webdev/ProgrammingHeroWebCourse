d46057302a7988064b11e9cc92fc21801ac4efc3
Jame___boy <jjame0030@gmail.com>
2024-05-09 15:14:47 +0600

    create database, collection and a path to get all jobs data

--- index.js
+++ index.js
@@ -39,6 +39,25 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
+
+        const database = client.db("soloSphere");
+        const jobsCollection = database.collection('jobs');
+        const bidsCollection = database.collection('bids');
+
+
+        //!----------------CRUD Start  -----------
+        // Get all jobs data from database
+        //* Find/Read all documents
+        // https://expressjs.com/en/starter/basic-routing.html
+        app.get('/jobs', async (req, res) => {
+            // https://www.mongodb.com/docs/drivers/node/current/usage-examples/find/
+            const result = await jobsCollection.find().toArray();
+            res.send(result);
+        })
+        //!----------------CRUD  End  ------------
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
