a392fd8b6016156019d261c92949b26511cbbea5
Jame___boy <jjame0030@gmail.com>
2024-05-11 01:48:02 +0600

    add handleDelete in MyPostedJobs.jsx

--- src/pages/MyPostedJobs.jsx
+++ src/pages/MyPostedJobs.jsx
@@ -4,11 +4,12 @@ import { AuthContext } from "../provider/AuthProvider";
 import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import toast from "react-hot-toast";
 
 const MyPostedJobs = () => {
     const { user } = useContext(AuthContext);
     // https://github.com/shakilahmedatik/soloSphere-session/blob/main/client/src/pages/MyPostedJobs.jsx
-    const [jobs, setJobs] = useState([]); 
+    const [jobs, setJobs] = useState([]);
 
     useEffect(() => {
         getData();
@@ -17,6 +18,20 @@ const MyPostedJobs = () => {
     const getData = async () => {
         const { data } = await axios(`${import.meta.env.VITE_API_URL}/jobs/${user?.email}`);
         setJobs(data);
+    };
+
+    const handleDelete = async (id) => {
+        try {
+            const { data } = await axios.delete(`${import.meta.env.VITE_API_URL}/job/${id}`);
+            console.log(data);
+            toast.success("Delete Successful");
+
+            //! refresh UI
+            getData();
+        } catch (err) {
+            console.log(err.message);
+            toast.error(err.message);
+        }
     };
 
     return (
@@ -84,11 +99,7 @@ const MyPostedJobs = () => {
                                             </td>
                                             <td className="px-4 py-4 text-sm whitespace-nowrap">
                                                 <div className="flex items-center gap-x-6">
-                                                    <button
-                                                        // onClick={() => handleDelete(job._id)}
-
-                                                        className="text-gray-500 transition-colors duration-200   hover:text-red-500 focus:outline-none"
-                                                    >
+                                                    <button onClick={() => handleDelete(job._id)} className="text-gray-500 transition-colors duration-200   hover:text-red-500 focus:outline-none">
                                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
                                                             <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                                         </svg>
