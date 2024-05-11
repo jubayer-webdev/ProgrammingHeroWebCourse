094bec04aef57801583cb7492023d3e1730a8921
Jame___boy <jjame0030@gmail.com>
2024-05-12 00:49:56 +0600

    set path to delete token when logout is happen in AuthProvider.jsx

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -4,6 +4,7 @@
 import { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
+import axios from "axios";
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
@@ -30,6 +31,13 @@ const AuthProvider = ({ children }) => {
 
     const logOut = async () => {
         setLoading(true);
+        
+        //! Clear token from Cookies
+        const { data } = await axios(`${import.meta.env.VITE_API_URL}/logout`, {
+            withCredentials: true,
+        });
+        console.log(data);
+
         return signOut(auth);
     };
 
