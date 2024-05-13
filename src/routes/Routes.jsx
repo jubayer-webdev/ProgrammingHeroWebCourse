9c8b0cf3d8577e67bc5c181b3ec24a9a2b42e17d
Jame___boy <jjame0030@gmail.com>
2024-05-14 00:08:00 +0600

    added AllJobs.jsx component initially and set its routes

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -13,6 +13,7 @@ import UpdateJob from "../pages/UpdateJob";
 import PrivateRoute from "./PrivateRoute";
 import MyBids from "../pages/MyBids";
 import BidRequests from "../pages/BidRequests";
+import AllJobs from "../pages/AllJobs";
 
 const router = createBrowserRouter([
     {
@@ -82,6 +83,10 @@ const router = createBrowserRouter([
                         <BidRequests />
                     </PrivateRoute>
                 ),
+            },
+            {
+                path: "/jobs",
+                element: <AllJobs />,
             },
         ],
     },
