c3b9b64a3643b87f347d5eca407e0be5d1882ed9
Jame___boy <jjame0030@gmail.com>
2024-05-22 00:40:38 +0600

    fixed active button issue by adding (end) property

--- src/components/Dashboard/Sidebar/Sidebar.jsx
+++ src/components/Dashboard/Sidebar/Sidebar.jsx
@@ -68,7 +68,12 @@ const Sidebar = () => {
                         {/*  //! Menu Items */}
                         <nav>
                             {/* //! Statistics */}
-                            <NavLink to="/dashboard" className={({ isActive }) => `flex items-center px-4 py-2 my-5  transition-colors duration-300 transform  hover:bg-gray-300   hover:text-gray-700 ${isActive ? "bg-gray-300  text-gray-700" : "text-gray-600"}`}>
+                            <NavLink
+                                //
+                                to="/dashboard"
+                                end
+                                className={({ isActive }) => `flex items-center px-4 py-2 my-5  transition-colors duration-300 transform  hover:bg-gray-300   hover:text-gray-700 ${isActive ? "bg-gray-300  text-gray-700" : "text-gray-600"}`}
+                            >
                                 <BsGraphUp className="w-5 h-5" />
 
                                 <span className="mx-4 font-medium">Statistics</span>
