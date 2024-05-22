1c198164a960930a94fedf5f276b945704e527b8
Jame___boy <jjame0030@gmail.com>
2024-05-22 06:54:40 +0600

    add functionality in AddRoom.jsx to submit AddRoomForm.jsx data, added a utility for upload image and get the url

--- src/components/Form/AddRoomForm.jsx
+++ src/components/Form/AddRoomForm.jsx
@@ -1,20 +1,11 @@
 import { DateRange } from "react-date-range";
 import { categories } from "../Categories/CategoriesData";
-import { useState } from "react";
 
-const AddRoomForm = () => {
-    const [state, setState] = useState([
-        {
-            startDate: new Date(),
-            endDate: null,
-            key: "selection",
-        },
-    ]);
-
+const AddRoomForm = ({ dates, handleDates, handleSubmit }) => {
     return (
         <div className="w-full min-h-[calc(100vh-40px)] flex flex-col justify-center items-center text-gray-800 rounded-xl bg-gray-50">
             {/* //!------------------------ Form Start ----------------------- */}
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
                     {/* //! Left side */}
                     <div className="space-y-6">
@@ -51,9 +42,9 @@ const AddRoomForm = () => {
                                 //
                                 rangeColors={["#F43F5E"]}
                                 editableDateInputs={true}
-                                onChange={(item) => setState([item.selection])}
+                                onChange={(item) => handleDates(item)}
                                 moveRangeOnFirstSelection={false}
-                                ranges={state}
+                                ranges={[dates]}
                             />
                         </div>
                     </div>
--- src/pages/Dashboard/Host/AddRoom.jsx
+++ src/pages/Dashboard/Host/AddRoom.jsx
@@ -1,22 +1,79 @@
 import { useState } from "react";
 import AddRoomForm from "../../../components/Form/AddRoomForm";
+import useAuth from "../../../hooks/useAuth";
+import { imageUpload } from "../../../api/utils";
 
 const AddRoom = () => {
+    const { user } = useAuth();
     const [dates, setDates] = useState({
         startDate: new Date(),
         endDate: new Date(),
-        key: "selection",
+        key: "selectionKEY",
     });
 
     //! Date range handler
     const handleDates = (item) => {
-        setDates(item.selection);
+        setDates(item.selectionKEY);
+        console.log("Date range in AddRooms item =", item);
+    };
+
+    //!   Form handler
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        // setLoading(true);
+        const form = e.target;
+
+        const location = form.location.value;
+        const category = form.category.value;
+        const title = form.title.value;
+        const to = dates.endDate;
+        const from = dates.startDate;
+        const price = form.price.value;
+        const guests = form.total_guest.value;
+        const bathrooms = form.bathrooms.value;
+        const description = form.description.value;
+        const bedrooms = form.bedrooms.value;
+        const image = form.image.files[0];
+
+        //! owner of the house who give the room
+        const host = {
+            name: user?.displayName,
+            image: user?.photoURL,
+            email: user?.email,
+        };
+
+        try {
+            //! image
+            const image_url = await imageUpload(image);
+            console.log("image_url =", image_url);
+
+            const roomData = {
+                location,
+                category,
+                title,
+                to,
+                from,
+                price,
+                guests,
+                bathrooms,
+                bedrooms,
+                host,
+                description,
+                image: image_url,
+            };
+            console.table(roomData);
+
+            //!   Post request to server
+        } catch (err) {
+            console.log(err);
+        }
     };
 
     return (
         <div>
             {/* Form */}
-            <AddRoomForm dates={dates} />
+            <AddRoomForm dates={dates} handleDates={handleDates} handleSubmit={handleSubmit} />
         </div>
     );
 };
